e08aabce4b2428f2979a6703376d3d30515f273a
isaias0412 <muhammadisaias@gmail.com>
2021-05-19 20:56:12 +0700

    Sistem Pelayanan Surat Menyurat Desa Sindangkerta
    Kelompok 7 fixx

---
+++ konek.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'surat';
+
+    $konek = mysqli_connect($hostname,$username,$password,$database);
+?>
